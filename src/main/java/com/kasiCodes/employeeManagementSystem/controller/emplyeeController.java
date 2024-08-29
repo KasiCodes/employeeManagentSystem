6540db7871ed8b4d3e680d542243d2bdc3460ca2
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-08-29 13:37:18 +0200

    added thymleaf template

--- src/main/java/com/kasiCodes/employeeManagementSystem/controller/emplyeeController.java
+++ src/main/java/com/kasiCodes/employeeManagementSystem/controller/emplyeeController.java
@@ -1,5 +1,27 @@
 package com.kasiCodes.employeeManagementSystem.controller;
 
+import com.kasiCodes.employeeManagementSystem.service.employeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
 public class emplyeeController {
+    @Autowired
+    private employeeService employeeService;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployee", employeeService.getAllEmployees());
+
+        return "index";
+
+    }
+
+
     
 }
